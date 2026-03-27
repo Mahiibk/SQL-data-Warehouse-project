b85a5343a914944f4a5874bed1a1a15afc13432e
Mahiibk <mahikorabu03@gmail.com>
2026-03-27 15:03:03 +0530

    Create Data integration.sql

---
+++ Scripts/gold_layer/Data integration.sql
@@ -0,0 +1,78 @@
+use DataWarehouse
+
+--checking the duplicates as primary key
+SELECT prd_key, count(*) FROM
+(
+SELECT
+pn.prd_id,
+pn.cat_id,
+pn.prd_key,
+pn.prd_nm,
+pn.prd_cost,
+pn.prd_line,
+pn.prd_start_dt,
+pc.cat,
+pc.subcat,
+pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL--filter out all historical data
+)t group by prd_key HAVING count(*) > 1 --NO duplicate datas
+
+-- grouping the relavant data and naming colum
+SELECT
+pn.prd_id as product_id,
+pn.prd_key as product_number,
+pn.prd_nm as product_name,
+pn.cat_id as category_id,
+pc.cat as category,
+pc.subcat as subcategory,
+pc.maintenance,
+pn.prd_cost as product_cost,
+pn.prd_line as product_line,
+pn.prd_start_dt as start_Date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL
+
+--creating a private key number
+SELECT
+ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category,
+	pc.subcat as subcategory,
+	pc.maintenance,
+	pn.prd_cost as product_cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_Date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL
+
+--Creating gold dimension view
+
+CREATE VIEW gold.dim_products as
+SELECT
+ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category,
+	pc.subcat as subcategory,
+	pc.maintenance,
+	pn.prd_cost as product_cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_Date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL
+
+SELECT * FROM gold.dim_products
